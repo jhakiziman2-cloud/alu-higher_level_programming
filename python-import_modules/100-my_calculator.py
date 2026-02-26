14eef7d7cba1f144429fe3798a55a77cf07a9c79
jhakiziman2-cloud <j.hakiziman2@alustudent.com>
2026-02-26 11:24:19 -0800

    calcu

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -8,21 +8,23 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
+    # Assign and cast arguments
     a = int(sys.argv[1])
     operator = sys.argv[2]
     b = int(sys.argv[3])
 
-    # Dictionary mapping operators to functions for clean logic
-    ops = {
-        "+": add,
-        "-": sub,
-        "*": mul,
-        "/": div
-    }
-
-    if operator not in ops:
+    # Handle operations based on the operator provided
+    if operator == "+":
+        result = add(a, b)
+    elif operator == "-":
+        result = sub(a, b)
+    elif operator == "*":
+        result = mul(a, b)
+    elif operator == "/":
+        result = div(a, b)
+    else:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
-    result = ops[operator](a, b)
+    # Print the result in the specified format
     print("{:d} {:s} {:d} = {:d}".format(a, operator, b, result))
